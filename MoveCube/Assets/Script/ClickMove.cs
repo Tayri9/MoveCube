266249564f2fd3038c03ecf7f86860a3576e798b
Tayri <tayrifernandez@gmail.com>
2022-11-17 08:25:35 +0000

    arreglado

--- MoveCube/Assets/Script/ClickMove.cs
+++ MoveCube/Assets/Script/ClickMove.cs
@@ -5,35 +5,27 @@ using UnityEngine;
 public class ClickMove : MonoBehaviour
 {
     private GameObject selectedObject;
-    Vector3 mOffset;
 
     void Update()
     {
-        //Condicional de click izquierdo del ratón
-        if (Input.GetMouseButtonDown(0))
+        if (Input.touchCount>=1 && Input.GetTouch(0).phase == TouchPhase.Ended || Input.GetMouseButtonUp(0))
         {
-            //Condicional de que el objeto clickado no es nulo
             if (selectedObject == null)
             {
                 Ray rayo = Camera.main.ScreenPointToRay(Input.mousePosition);
                 RaycastHit hitInfo;
                 if (Physics.Raycast(rayo, out hitInfo))
-                {
-                    //Condicional de que el collider no es nulo
+                {                    
                     if (hitInfo.collider != null)
-                    {
-                        //Condicional de que el objeto clickado tiene la tag Clickable
+                    {                        
                         if (!hitInfo.collider.CompareTag("Object"))
-                        {                           
-                            //Si no la tiene sale de la función
+                        {                       
                             return;
                         }
-
-                        //Convertimos nuestro objeto clickado en el selectedObject
+                                                
                         selectedObject = hitInfo.collider.gameObject;
                         selectedObject.SetActive(false);
-
-                        //Desabilitamos la vista del cursor
+                        
                         Cursor.visible = false;
                     }
                 }
@@ -44,31 +36,25 @@ public class ClickMove : MonoBehaviour
                 RaycastHit hitInfo2;
                 if (Physics.Raycast(rayo2, out hitInfo2))
                 {
-                    mOffset = new Vector3(0f, selectedObject.transform.position.y, -0.75f);
-                    var tmpPos = mOffset;
-
                     selectedObject.SetActive(true);
-                    selectedObject.transform.position = hitInfo2.point + tmpPos;
+                    selectedObject.transform.position = hitInfo2.point * selectedObject.transform.localScale.y/2;
                 }
                 selectedObject = null;
                 Cursor.visible = true;
             }
         }
-
-        //Condicional de que el objeto clickado no es igual a nulo
+        
         if (selectedObject != null)
         {
+            selectedObject.SetActive(false);
             Ray rayo2 = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hitInfo2;
             if (Physics.Raycast(rayo2, out hitInfo2))
-            {
-                //mOffset = new Vector3(0f, selectedObject.transform.position.y, -0.75f);
-                //var tmpPos = mOffset;
-                
+            {                
                 selectedObject.SetActive(true);
-                selectedObject.transform.position = hitInfo2.point;// + tmpPos;
+                selectedObject.transform.position = hitInfo2.point + Vector3.up * selectedObject.transform.localScale.y/2;
             }
-
+            selectedObject.SetActive(true);
         }
     }
    
